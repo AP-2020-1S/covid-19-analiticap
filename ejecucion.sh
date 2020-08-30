4f0d7738fc7bcdb9da8cc20f04e812abc91cb4d2
Valentina <53501522+Vamarinpe@users.noreply.github.com>
2020-08-29 21:32:06 -0500

    Add files via upload

--- ejecucion.sh
+++ ejecucion.sh
@@ -1,24 +1,11 @@
 #!/bin/bash
 git clone https://github.com/AP-2020-1S/covid-19-analiticap analiticap
 ls -a
+cd \analiticap
 python3 ejecucionpy.py
 git status
 git config user.email "you@example.com"
 git config user.name "Your Name"
-git add <images/1.png>
-git add <images/2.png>
-git add <images/3.png>
-git add <images/4.png>
-git add <images/5.png>
-git add <images/6.png>
-git add <images/7.png>
-git add <images/8.png>
-git commit <images/1.png>
-git commit <images/2.png>
-git commit <images/3.png>
-git commit <images/4.png>
-git commit <images/5.png>
-git commit <images/6.png>
-git commit <images/7.png>
-git commit <images/8.png>
+git add <images>/*
+git commit <images>/*
 git push origin
