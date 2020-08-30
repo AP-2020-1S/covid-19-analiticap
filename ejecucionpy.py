18cc76493933059afdccfa312ac1a6c56d50b773
Valentina <53501522+Vamarinpe@users.noreply.github.com>
2020-08-29 20:45:11 -0500

    Add files via upload

---
+++ ejecucionpy.py
@@ -0,0 +1,115 @@
+print("Estoy ejecutando")
+
+import pandas as pd 
+
+data = [[45939, 21574, 2876, 1815, 1646,   89,  555],
+        [60423, 29990, 4708, 2568, 2366, 1411,  733],
+        [64721, 32510, 5230, 2695, 2526, 1546,  773],
+        [68484, 35218, 6662, 2845, 2691, 1663,  836],
+        [71799, 37598, 6856, 3000, 2868, 1769,  911],
+        [76036, 40341, 8220, 3145, 3054, 1905, 1008],
+        [79831, 43173, 9053, 3338, 3224, 2005, 1076]]
+
+
+
+data = pd.DataFrame(
+    data = data,
+    index = [2020, 1956, 2020, 1958, 1959, 202000, 202000],
+    columns = ['1', '2', '3', 'S.Amer', 'Oceania', 'Africa', 'Mid.Amer']
+)
+
+for col in data.columns:
+    fig = data.plot.bar(y=col).get_figure().savefig('images/' + col + '.png')
+"""
+import requests
+from pandas.io.json import json_normalize
+import pandas as pd
+url="https://www.datos.gov.co/resource/gt2j-8ykr.json?$limit=1000000000"
+Datos=pd.read_json(url,convert_dates=['fecha_de_notificaci_n','fecha_de_muerte','fis','fecha_diagnostico','fecha_recuperado','fecha_reporte_web'])
+
+Datos.rename(columns={'id_de_caso':'id_caso','fecha_de_notificaci_n':'fecha_notificacion','c_digo_divipola':'codigo_municipio','ciudad_de_ubicaci_n':'ciudad',
+                      'atenci_n':'atencion','tipo':'tipo_contagio','estado':'estado','pa_s_de_procedencia':'pais_procedencia','fis':'fecha_sintomas',
+                      'tipo_recuperaci_n':'tipo_recuperacion','fecha_de_muerte':'fecha_muerte'},inplace=True)
+
+import requests
+from pandas.io.json import json_normalize
+import pandas as pd
+
+url="https://www.datos.gov.co/resource/gt2j-8ykr.json?$limit=1000000000"
+Datos=pd.read_json(url,convert_dates=['fecha_de_notificaci_n','fecha_de_muerte','fis','fecha_diagnostico','fecha_recuperado','fecha_reporte_web'])
+
+Datos.rename(columns={'id_de_caso':'id_caso','fecha_de_notificaci_n':'fecha_notificacion','c_digo_divipola':'codigo_municipio','ciudad_de_ubicaci_n':'ciudad',
+                      'atenci_n':'atencion','tipo':'tipo_contagio','estado':'estado','pa_s_de_procedencia':'pais_procedencia','fis':'fecha_sintomas',
+                      'tipo_recuperaci_n':'tipo_recuperacion','fecha_de_muerte':'fecha_muerte'},inplace=True)
+
+# filtrando ciudades
+Datos1=Datos[Datos['ciudad'].isin(['Bogotá D.C.','Medellín','Barranquilla','Cali','Cartagena de Indias'])]
+Datos1.reset_index(inplace=True,drop=True)
+
+# tabla con ciudades filtradas para modelar casos nuevos
+tabla_nuevos=pd.pivot_table(Datos1,index=['fecha_reporte_web'],values=['id_caso'],aggfunc='count')
+tabla_nuevos=pd.DataFrame(tabla_nuevos.to_records())
+tabla_nuevos.rename(columns={'id_caso':'casos_nuevos_reportados'},inplace=True)
+tabla_nuevos.loc[:,'casos_confirmados']=tabla_nuevos['casos_nuevos_reportados'].cumsum()
+
+# los casos confirmados no pueden ser con modelo basado en la tendencia, se tiene que predecir con el resultado de la suma de los casos nuevos reportados
+tabla_nuevos.plot(x='fecha_reporte_web',y='casos_confirmados',figsize=(10, 5)).figure.savefig('images/1.png')
+
+tabla_nuevos.plot(x='fecha_reporte_web',y='casos_nuevos_reportados',figsize=(10, 5)).figure.savefig('images/2.png')
+
+from scipy import stats
+import statsmodels.api as sm
+from statsmodels.graphics.api import qqplot
+import matplotlib.pyplot as plt
+from matplotlib import pyplot
+import numpy as np
+from statsmodels.tsa.seasonal import seasonal_decompose
+
+# Serie normal
+M1Nuevos=tabla_nuevos.set_index('fecha_reporte_web')
+M1Nuevos=M1Nuevos.drop(['casos_confirmados'],axis=1)
+
+# Como varianza no constante, transformación log para estabilizar varianza
+M1NuevosLog=np.log(M1Nuevos)
+
+p_train=0.80
+p_test=0.12
+p_forecast=0.08
+M1NuevosLogTrain=M1NuevosLog[:int(len(M1NuevosLog)*p_train)]
+M1NuevosLogTest=M1NuevosLog[int(len(M1NuevosLog)*p_train):int(len(M1NuevosLog)*(p_train+p_test))]
+M1NuevosLogForecast=M1NuevosLog[int(len(M1NuevosLog)*(p_train+p_test)):]
+
+# Modelo identificado por la identificación automática
+from pmdarima import auto_arima
+arima=auto_arima(M1NuevosLogTrain)
+
+M1NuevosLogTrain['casos_nuevos_reportados_fit']=arima.predict_in_sample()
+plot=M1NuevosLogTrain[['casos_nuevos_reportados', 'casos_nuevos_reportados_fit']].plot(figsize=(10, 5)).figure.savefig('images/3.png')
+
+plt.figure(figsize=(10,5))
+plt.scatter(M1NuevosLogTrain['casos_nuevos_reportados'],M1NuevosLogTrain['casos_nuevos_reportados_fit'])
+plt.savefig('images/4.png')
+
+M1NuevosLogTest['casos_nuevos_reportados_test']=arima.predict(n_periods=(len(M1NuevosLogTest))) 
+plot=M1NuevosLogTest[['casos_nuevos_reportados', 'casos_nuevos_reportados_test']].plot(figsize=(10,5)).figure.savefig('images/5.png')
+
+plt.figure(figsize=(10,5))
+plt.scatter(M1NuevosLogTest['casos_nuevos_reportados'],M1NuevosLogTest['casos_nuevos_reportados_test'])
+plt.savefig('images/6.png')
+
+FM1Nuevos=np.exp(M1NuevosLogTrain)
+plt.plot(FM1Nuevos['casos_nuevos_reportados'])
+plt.plot(FM1Nuevos['casos_nuevos_reportados_fit'])
+plt.legend(['casos_nuevos_reportados','casos_nuevos_reportados_fit'])
+plt.savefig('images/7.png')
+
+FM1Nuevos=np.exp(M1NuevosLogTest)
+plt.plot(FM1Nuevos['casos_nuevos_reportados'])
+plt.plot(FM1Nuevos['casos_nuevos_reportados_test'])
+plt.legend(['casos_nuevos_reportados','casos_nuevos_reportados_test'])
+plt.savefig('images/8.png')
+"""
+
+print("terminé")
+
+
